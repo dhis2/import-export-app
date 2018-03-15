1be927c599564c6754a5b1368d283045ac8be6d5
Muhammad Adeel <adeel@dhis2.org>
2018-03-15 13:26:32 +0100

    style
    - add favicon
    - change webpage title
    - change frontend PORT

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
+import styled from 'styled-components'
+
+const Todo = styled.pre`
+  margin: 20px auto 20px auto;
+  width: 50%;
+  white-space: pre-wrap;
+  font-size: 18px;
+`
+
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        App
+        <Todo>{`
+TODO
+
+1. Authenticate
+2. Accumulate REST endpoints
+`}</Todo>
       </div>
     )
   }
