12988c749d0ee229e168f8f651ec84e9e8aba70a
Muhammad Adeel <imadeelshahid@gmail.com>
2018-03-21 12:31:04 +0100

    clean up - remove base64 encoding - remove OAuth, BasicAuth - only use withCreditionals on requests

--- src/config.js
+++ src/config.js
@@ -1,13 +1,4 @@
-export const AUTH_TYPE_BASIC_AUTH = 'auth/basicAuth'
-export const AUTH_TYPE_OAUTH2 = 'auth/oAuth2'
-
 export const apiConfig = {
   server: 'http://localhost:8080',
   version: '29',
-
-  auth: {
-    type: AUTH_TYPE_BASIC_AUTH,
-    username: 'admin',
-    password: 'district'
-  }
 }
--- src/services/API.js
+++ src/services/API.js
@@ -1,28 +1,9 @@
 import axios from 'axios'
-import { base64Encode } from 'helpers'
-import { AUTH_TYPE_BASIC_AUTH, AUTH_TYPE_OAUTH2 } from 'config'
 import { apiConfig } from 'config'
 
 export default class API {
   constructor() {
     this.config = apiConfig
-  }
-
-  authHeaders() {
-    const { type } = this.config.auth
-    if (type === AUTH_TYPE_BASIC_AUTH) {
-      const { username, password } = this.config.auth
-      const encoded = base64Encode(`${username}:${password}`)
-      return {
-        Authorization: `Basic ${encoded}`
-      }
-    } else if (type === AUTH_TYPE_OAUTH2) {
-      // TODO
-      console.warn('NOT IMPLEMENTED', AUTH_TYPE_OAUTH2)
-      return {}
-    } else {
-      console.error('Invalid Auth Type', type)
-    }
   }
 
   getBaseURL() {
@@ -36,9 +17,6 @@ export default class API {
       method: 'get',
       baseURL: this.getBaseURL(),
       withCredentials: true,
-      headers: {
-        ...this.authHeaders(),
-      }
     })
   }
 }
