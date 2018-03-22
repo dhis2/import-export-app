9b1355bb81e1dbe62ac593a15fd0e201a4d68862
Muhammad Adeel <imadeelshahid@gmail.com>
2018-03-22 14:12:38 +0100

    feat(integrate react-router): use hash routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { HashRouter as Router } from 'react-router-dom'
 
 import './index.css'
 import App from './App'
@@ -9,7 +10,9 @@ import { store } from './store'
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('root'),
 )
