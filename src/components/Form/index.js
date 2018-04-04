02e7d3e6304314e54c692b3419137941fa7ac198
Muhammad Adeel <adeel@dhis2.org>
2018-04-04 12:38:50 +0200

    structure: fields for Import MetaData
    - export constant for form field types from Form
    - use Context which can be used to show/hide fields

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import s from './styles.css'
 
+export const TYPE_FILE = 'fieldType/FILE'
+export const TYPE_TEXT = 'fieldType/TEXT'
+export const TYPE_SELECT = 'fieldType/SELECT'
+
+export const CTX_DEFAULT = 'ctx/DEFAULT'
+export const CTX_MORE_OPTIONS = 'ctx/MORE_OPTIONS'
+
 export class Form extends React.Component {
   render() {
     const { title, style } = this.props
--- src/pages/import/MetaData.js
+++ src/pages/import/MetaData.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import { Select, MenuItem, FormControl, InputLabel } from 'material-ui'
 import i18n from 'd2-i18n'
-import { Form } from 'components'
+import {
+  Form,
+  CTX_DEFAULT,
+  CTX_MORE_OPTIONS,
+  TYPE_FILE,
+  TYPE_SELECT,
+} from 'components'
 
 const attrs = {
   formControl: {
@@ -20,6 +26,105 @@ export class MetaDataImport extends React.Component {
   static description = i18n.t(
     'Import metadata like data elements and organisation units using the standard DHIS 2 exchange format called DXF 2.',
   )
+
+  fields = [
+    {
+      context: CTX_DEFAULT,
+      type: TYPE_FILE,
+      name: 'file',
+      label: i18n.t('File'),
+    },
+    {
+      context: CTX_DEFAULT,
+      type: TYPE_SELECT,
+      name: 'importFormat',
+      label: i18n.t('Format'),
+    },
+    {
+      context: CTX_DEFAULT,
+      type: TYPE_SELECT,
+      name: 'dryRun',
+      label: i18n.t('Dry run'),
+    },
+    {
+      context: CTX_DEFAULT,
+      type: TYPE_SELECT,
+      name: 'strategy',
+      label: i18n.t('Strategy'),
+    },
+    {
+      context: CTX_MORE_OPTIONS,
+      type: TYPE_SELECT,
+      name: 'atomicMode',
+      label: i18n.t('Reference mode'),
+    },
+  ]
+
+  state = {
+    format: {
+      selected: 'json',
+      values: [
+        {
+          value: 'json',
+          label: i18n.t('JSON'),
+        },
+        {
+          value: 'xml',
+          label: i18n.t('XML'),
+        },
+        {
+          value: 'csv',
+          label: i18n.t('CSV'),
+        },
+      ],
+    },
+
+    dryRun: {
+      selected: 'false',
+      values: [
+        {
+          value: 'false',
+          label: i18n.t('No'),
+        },
+        {
+          value: 'true',
+          label: i18n.t('Yes'),
+        },
+      ],
+    },
+
+    strategy: {
+      selected: 'NEW_AND_UPDATES',
+      values: [
+        {
+          value: 'NEW_AND_UPDATES',
+          label: i18n.t('New and Updates'),
+        },
+        {
+          value: 'NEW',
+          label: i18n.t('New only'),
+        },
+        {
+          value: 'UPDATES',
+          label: i18n.t('Updates only'),
+        },
+      ],
+    },
+
+    atomicMode: {
+      selected: 'NONE',
+      values: [
+        {
+          value: 'NONE',
+          label: i18n.t('Allow invalid references'),
+        },
+        {
+          value: 'ALL',
+          label: i18n.t('Deny invalid references'),
+        },
+      ],
+    },
+  }
 
   onSubmit = () => {
     console.log('onSubmit')
