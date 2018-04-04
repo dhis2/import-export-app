e75fa948479954d8f2eac9f09e04c33345fb7853
Muhammad Adeel <adeel@dhis2.org>
2018-04-04 13:37:18 +0200

    structure: GML Import form fields

--- src/pages/import/GML.js
+++ src/pages/import/GML.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import i18n from 'd2-i18n'
+import { CTX_DEFAULT, TYPE_FILE, TYPE_SELECT } from '../../components'
 
 export class GMLImport extends React.Component {
   static path = '/import/gml'
@@ -9,6 +10,37 @@ export class GMLImport extends React.Component {
   static description = i18n.t(
     'Import geographic data for organisation units using GML format. GML is an XML grammar for expressing geographical features.',
   )
+
+  fields = [
+    {
+      context: CTX_DEFAULT,
+      type: TYPE_FILE,
+      name: 'file',
+      label: i18n.t('File'),
+    },
+    {
+      context: CTX_DEFAULT,
+      type: TYPE_SELECT,
+      name: 'dryRun',
+      label: i18n.t('Dry run'),
+    },
+  ]
+
+  state = {
+    dryRun: {
+      selected: 'false',
+      values: [
+        {
+          value: 'false',
+          label: i18n.t('No'),
+        },
+        {
+          value: 'true',
+          label: i18n.t('Yes'),
+        },
+      ],
+    },
+  }
 
   render() {
     return <div>import gml</div>
