8fe9cfba691757175837b2f6901bc2b238fed790
Muhammad Adeel <adeel@dhis2.org>
2018-04-23 10:28:15 +0200

    test: include store + routing in App.test
    - upgrade jest
    - encapsulate App in redux store and routing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
+import { store } from './store'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(
+    <Provider store={store}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Provider>,
+    div
+  )
   ReactDOM.unmountComponentAtNode(div)
 })
