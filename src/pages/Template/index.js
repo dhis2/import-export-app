b276ba1a0c1fb352a102a2c6ba9a5f2715319623
Muhammad Adeel <imadeelshahid@gmail.com>
2018-05-11 12:36:55 +0200

    integrate: d2-header bar

--- src/pages/Template/index.js
+++ src/pages/Template/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import HeaderBar from '@dhis2/d2-ui-header-bar'
 import { SidePanel } from 'components'
 import s from './styles.css'
 
 export default class Template extends React.Component {
+  static contextTypes = {
+    d2: PropTypes.object
+  }
+
   render() {
     return (
       <div className={s.container}>
+        <HeaderBar d2={this.context.d2} />
         <SidePanel />
         <div className={s.content}>{this.props.children}</div>
       </div>
