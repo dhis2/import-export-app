c56d7f5e94bc6ce54d286acbdaa9c80c71d30e54
Muhammad Adeel <adeel@dhis2.org>
2018-05-15 14:26:09 +0200

    feat(Blob): helper functions
    - create blob
    - download blob
    - get blob file type based on compression / format

---
+++ src/helpers/blob.js
@@ -0,0 +1,25 @@
+export function blobType(format, compression) {
+  if (compression === 'gzip') {
+    return 'application/json'
+  } else if (compression === 'zip') {
+    return 'application/zip'
+  }
+
+  if (format === 'xml') {
+    return 'application/xml'
+  } else if (format === 'json') {
+    return 'application/json'
+  }
+}
+
+export function createBlob(contents, format, compression) {
+  return URL.createObjectURL(new Blob([contents], { type: blobType(format, compression) }))
+}
+
+export function downloadBlob(url, filename) {
+  const link = document.createElement('a')
+  link.href = url
+  link.setAttribute('download', filename)
+  document.body.appendChild(link)
+  link.click()
+}
