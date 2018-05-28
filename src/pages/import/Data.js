f533761e4599cc16ff3603845eed193906c84616
Muhammad Adeel <adeel@dhis2.org>
2018-05-28 12:42:34 +0200

    DataImport(Logger): fetch log progress

--- src/pages/import/Data.js
+++ src/pages/import/Data.js
@@ -1,6 +1,6 @@
 import i18n from '@dhis2/d2-i18n'
 import { apiConfig } from 'config'
-import { eventEmitter } from 'services'
+import { api, eventEmitter } from 'services'
 import {
   FormBase,
   CTX_DEFAULT,
@@ -234,6 +234,46 @@ export class DataImport extends FormBase {
     }
   }
 
+  componentDidMount() {
+    this.fetchLog()
+  }
+
+  lastId = null
+  fetchLog = async () => {
+    try {
+      let url = '../system/tasks/DATAVALUE_IMPORT'
+      if (this.lastId) {
+        url += `?lastId=${this.lastId}`
+      }
+      const { data } = await api.get(url)
+
+      if (data.length > 0) {
+        this.lastId = data[0]['uid']
+
+        for (let i = data.length - 1; i >= 0; i -= 1) {
+          const { category, completed, level, message, time, uid } = data[i]
+          eventEmitter.emit('log', {
+            id: uid,
+            d: new Date(time),
+            subject: 'Data Import',
+            text: `Completed: ${completed}
+Level: ${level}
+Category: ${category}
+Message:
+${message}`
+          })
+        }
+        eventEmitter.emit('log.open')
+
+        if (data.filter(item => item.completed).length > 0) {
+          clearInterval(this.interval)
+        }
+      }
+    } catch (e) {
+      console.log('Error fetching DATAVALUE_IMPORT')
+    }
+  }
+
   onSubmit = () => {
     try {
       const {
@@ -275,6 +315,8 @@ Skip existing record check: ${skipExistingCheck}`
       eventEmitter.emit('log.open')
 
       this.setState({ processing: true })
+      this.interval = setInterval(this.fetchLog, 2000)
+
       window
         .fetch(
           `${apiConfig.server}/dhis-web-importexport/importDataValue.action`,
