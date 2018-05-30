639a2291be40e5d94c9d4c0293b988059582845e
Muhammad Adeel <adeel@dhis2.org>
2018-05-30 09:06:49 +0200

    tests(UserReducer)
    - USER_CLEAR
    - USER_SET

---
+++ src/reducers/user/__tests__/user.test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import userReducer from '../'
+import { USER_SET, USER_CLEAR } from '../actions'
+
+describe('User Reducer', () => {
+  it('USER_CLEAR removes existing user', () => {
+    expect(userReducer(null, { type: USER_CLEAR })).toBe(null)
+  })
+
+  it('USER_SET sets user', () => {
+    const user = { name: 'Muhammad Adeel' }
+    expect(userReducer(null, { type: USER_SET, payload: user })).toMatchObject(
+      user
+    )
+  })
+})
