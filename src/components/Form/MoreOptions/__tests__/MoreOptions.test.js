50ea936ffe4df40c29420c57af9474c119153226
Muhammad Adeel <adeel@dhis2.org>
2018-05-30 07:14:03 +0200

    tests(Enzyme): configuration
    - add Enzyme setup outside tests cases inside config/
    - remove Enzyme setup from Loading and MoreOptions test files

--- src/components/Form/MoreOptions/__tests__/MoreOptions.test.js
+++ src/components/Form/MoreOptions/__tests__/MoreOptions.test.js
@@ -1,27 +1,22 @@
-import React from 'react'
-import Adapter from 'enzyme-adapter-react-16'
-import { configure, mount } from 'enzyme'
-
-import { Loading } from '../'
+import { mount } from 'enzyme'
+import MoreOptions from '../'
 
 import { MuiThemeProvider } from 'material-ui'
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 const muiTheme = getMuiTheme(lightBaseTheme)
 
-configure({ adapter: new Adapter() })
-
 let wrapper
 beforeAll(() => {
   wrapper = mount(
     <MuiThemeProvider muiTheme={muiTheme}>
-      <Loading />
+      <MoreOptions />
     </MuiThemeProvider>
   )
 })
 
-describe('Loading', () => {
-  it('contains svg elm. <circle />', () => {
-    expect(wrapper.find('circle').exists()).toBe(true)
+describe('FormBase', () => {
+  it('contains <div/>', () => {
+    expect(wrapper.find('div').exists()).toBe(true)
   })
 })
--- src/components/Loading/__tests__/Loading.test.js
+++ src/components/Loading/__tests__/Loading.test.js
@@ -1,27 +1,24 @@
 import React from 'react'
-import Adapter from 'enzyme-adapter-react-16'
-import { configure, mount } from 'enzyme'
+import { mount } from 'enzyme'
 
-import MoreOptions from '../'
+import { Loading } from '../'
 
 import { MuiThemeProvider } from 'material-ui'
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 const muiTheme = getMuiTheme(lightBaseTheme)
 
-configure({ adapter: new Adapter() })
-
 let wrapper
 beforeAll(() => {
   wrapper = mount(
     <MuiThemeProvider muiTheme={muiTheme}>
-      <MoreOptions />
+      <Loading />
     </MuiThemeProvider>
   )
 })
 
-describe('FormBase', () => {
-  it('contains <div/>', () => {
-    expect(wrapper.find('div').exists()).toBe(true)
+describe('Loading', () => {
+  it('contains svg elm. <circle />', () => {
+    expect(wrapper.find('circle').exists()).toBe(true)
   })
 })
