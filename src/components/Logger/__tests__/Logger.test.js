04800af205022e3f971ac59dc767763fb10622e8
Muhammad Adeel <adeel@dhis2.org>
2018-05-30 08:50:08 +0200

    tests(Logger)

---
+++ src/components/Logger/__tests__/Logger.test.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Logger } from '../'
+
+const wrapper = mount(<Logger />)
+wrapper.setState({
+  open: true,
+  list: [
+    {
+      id: new Date().getTime(),
+      d: new Date(),
+      subject: 'Subject',
+      text: 'some message'
+    }
+  ]
+})
+
+describe('Logger', () => {
+  it('Title', () => {
+    expect(wrapper.containsMatchingElement(<span>Logger</span>)).toBe(true)
+  })
+
+  it('Description', () => {
+    expect(
+      wrapper.containsMatchingElement(
+        <span>view messages received on using import/export forms.</span>
+      )
+    ).toBe(true)
+  })
+
+  it('Log message contents', () => {
+    expect(wrapper.containsMatchingElement(<div>Subject</div>)).toBe(true)
+    expect(wrapper.containsMatchingElement(<div>some message</div>)).toBe(true)
+  })
+
+  it('Log is empty', () => {
+    wrapper.setState({
+      open: false,
+      list: []
+    })
+    expect(wrapper.html()).toBe(null)
+  })
+})
--- src/components/SidePanel/__tests__/SidePanel.test.js
+++ src/components/SidePanel/__tests__/SidePanel.test.js
@@ -9,35 +9,37 @@ const wrapper = mount(
 )
 
 describe('SidePanel', () => {
-  it ('Link to Metadata Import', () => {
+  it('Link to Metadata Import', () => {
     expect(wrapper.find('a[href="#/import/metadata"]').length).toEqual(1)
   })
 
-  it ('Link to Data Import', () => {
+  it('Link to Data Import', () => {
     expect(wrapper.find('a[href="#/import/data"]').length).toEqual(1)
   })
 
-  it ('Link to GML Import', () => {
+  it('Link to GML Import', () => {
     expect(wrapper.find('a[href="#/import/gml"]').length).toEqual(1)
   })
 
-  it ('Link to Event Import', () => {
+  it('Link to Event Import', () => {
     expect(wrapper.find('a[href="#/import/event"]').length).toEqual(1)
   })
 
-  it ('Link to Metadata Export', () => {
+  it('Link to Metadata Export', () => {
     expect(wrapper.find('a[href="#/export/metadata"]').length).toEqual(1)
   })
 
-  it ('Link to Metadata Dependency Export', () => {
-    expect(wrapper.find('a[href="#/export/metadata-dependency"]').length).toEqual(1)
+  it('Link to Metadata Dependency Export', () => {
+    expect(
+      wrapper.find('a[href="#/export/metadata-dependency"]').length
+    ).toEqual(1)
   })
 
-  it ('Link to Data Export', () => {
+  it('Link to Data Export', () => {
     expect(wrapper.find('a[href="#/export/data"]').length).toEqual(1)
   })
 
-  it ('Link to Event Export', () => {
+  it('Link to Event Export', () => {
     expect(wrapper.find('a[href="#/export/event"]').length).toEqual(1)
   })
 })
