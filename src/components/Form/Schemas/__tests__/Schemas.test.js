9ae8b229f8e7d6451d78a897de46a03c74bd0cb5
Muhammad Adeel <imadeelshahid@gmail.com>
2018-06-05 12:12:50 +0200

    Schemas(Test): empty render with Loading component

---
+++ src/components/Form/Schemas/__tests__/Schemas.test.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import Schemas from '../'
+import { Loading } from '../../../Loading'
+
+import { MuiThemeProvider } from 'material-ui'
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+
+const muiTheme = getMuiTheme(lightBaseTheme)
+
+describe('Schemas', () => {
+  it('empty render', () => {
+    const wrapper = mount(
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <Schemas name="schema" label="Schema" />
+      </MuiThemeProvider>
+    )
+
+    expect(wrapper.containsAllMatchingElements([<Loading />])).toEqual(true)
+  })
+})
