b5476c42bf27db9aa8f0372ca81bc77a87620927
Muhammad Adeel <imadeelshahid@gmail.com>
2018-06-05 12:30:59 +0200

    Helpers(getField): check correct field being returned from fields list

--- src/helpers/__tests__/helpers.test.js
+++ src/helpers/__tests__/helpers.test.js
@@ -1,4 +1,5 @@
 import { today } from '../date'
+import { getField } from '../form'
 
 describe('Date', () => {
   it('today() is not null', () => {
@@ -9,5 +10,18 @@ describe('Date', () => {
   it('today() is a date object', () => {
     const d = today()
     expect(typeof d.getMonth === 'function').toEqual(true)
+  })
+})
+
+describe('Form', () => {
+  it('getField', () => {
+    const fields = [
+      {
+        name: 'test'
+      }
+    ]
+    const selected = 'test'
+    const f = getField(selected, fields)
+    expect(f.name === selected).toBe(true)
   })
 })
