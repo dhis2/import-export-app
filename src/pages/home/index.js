0ffe4cb157ecf2ff48ca57c9a2b5f2518328954a
Muhammad Adeel <adeel@dhis2.org>
2018-06-19 12:10:37 +0200

    move homepage title/descriptions into render

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,25 +4,16 @@ import { withRouter } from 'react-router-dom'
 import { LogoIcon } from 'components/Icon'
 import s from './styles.css'
 
-import * as importPages from 'pages/import'
-import * as exportPages from 'pages/export'
-
-let list = []
-function addToList(key, pages) {
-  const p = pages[key]
-  list.push({
-    path: p.path,
-    order: p.order,
-    title: p.title,
-    menuIcon: p.menuIcon,
-    description: p.description
-  })
-}
-
-Object.keys(importPages).forEach(k => addToList(k, importPages))
-Object.keys(exportPages).forEach(k => addToList(k, exportPages))
-
-list = list.sort((a, b) => a.order > b.order)
+import {
+  MetaDataImport,
+  MetaDataExport,
+  DataImport,
+  DataExport,
+  GMLImport,
+  EventImport,
+  EventExport,
+  MetaDataDependencyExport
+} from 'pages'
 
 const Item = ({ path, title, description, menuIcon, onClick }) => (
   <div className={s.item} onClick={() => onClick(path)}>
@@ -41,6 +32,82 @@ export class Home extends React.Component {
   onClick = path => this.props.history.push(path)
 
   render() {
+    const list = [
+      {
+        path: MetaDataImport.path,
+        order: MetaDataImport.order,
+        menuIcon: MetaDataImport.menuIcon,
+        title: i18n.t('Metadata Import'),
+        description: i18n.t(
+          'Import metadata like data elements and organisation units using the standard DHIS 2 exchange format called DXF 2.'
+        )
+      },
+      {
+        path: DataImport.path,
+        order: DataImport.order,
+        menuIcon: DataImport.menuIcon,
+        title: i18n.t('Data Import'),
+        description: i18n.t(
+          'Import data values on the DXF 2 XML, JSON, CSV and PDF formatrant s. DXF 2 is the standard exchange format for DHIS 2.'
+        )
+      },
+      {
+        path: EventImport.path,
+        order: EventImport.order,
+        menuIcon: EventImport.menuIcon,
+        title: i18n.t('Event Import'),
+        description: i18n.t(
+          'Import events for programs, stages and tracked entities in the DXF 2 format.'
+        )
+      },
+      {
+        path: GMLImport.path,
+        order: GMLImport.order,
+        menuIcon: GMLImport.menuIcon,
+        title: i18n.t('GML Import'),
+        description: i18n.t(
+          'Import geographic data for organisation units using GML format. GML is an XML grammar for expressing geographical features.'
+        )
+      },
+
+      {
+        path: MetaDataExport.path,
+        order: MetaDataExport.order,
+        menuIcon: MetaDataExport.menuIcon,
+        title: i18n.t('Metadata Export'),
+        description: i18n.t(
+          'Export meta data like data elements and organisation units to the standard DHIS 2 exchange format.'
+        )
+      },
+      {
+        path: DataExport.path,
+        order: DataExport.order,
+        menuIcon: DataExport.menuIcon,
+        title: i18n.t('Data Export'),
+        description: i18n.t(
+          'Export data values. This is the regular export function which exports data to the DHIS 2 exchange format called DXF 2.'
+        )
+      },
+      {
+        path: EventExport.path,
+        order: EventExport.order,
+        menuIcon: EventExport.menuIcon,
+        title: i18n.t('Event Export'),
+        description: i18n.t(
+          'Export event data for programs, stages and tracked entities in the DXF 2 format.'
+        )
+      },
+      {
+        path: MetaDataDependencyExport.path,
+        order: MetaDataDependencyExport.order,
+        menuIcon: MetaDataDependencyExport.menuIcon,
+        title: i18n.t('Metadata Dependency Export'),
+        description: i18n.t(
+          'Export metadata like data sets and programs including related metadata objects.'
+        )
+      }
+    ]
+
     return (
       <div className={s.container}>
         <div className={s.logo}>
