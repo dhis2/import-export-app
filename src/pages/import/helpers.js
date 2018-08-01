ab56261efd5430616d16714c53d3878c1c548251
Muhammad Adeel <imadeelshahid@gmail.com>
2018-08-01 17:29:14 +0300

    on multiple imports log records come as Object of keys containing arrays
    - tackle log records if array
    - tackle log records if object with keys containing arrays

--- src/pages/import/helpers.js
+++ src/pages/import/helpers.js
@@ -20,6 +20,23 @@ const typeLabel = {
   EVENT_IMPORT: 'Event Import',
   GML_IMPORT: 'GML Import'
 }
+
+function emitLog(data, type) {
+  for (let i = data.length - 1; i >= 0; i -= 1) {
+    const { category, completed, level, message, time, uid } = data[i]
+    eventEmitter.emit('log', {
+      id: uid,
+      d: new Date(time),
+      subject: typeLabel[type],
+      text: `Completed: ${completed}
+Level: ${level}
+Category: ${category}
+Message:
+${message}`
+    })
+  }
+}
+
 export async function fetchLog(type) {
   try {
     let path = `system/tasks/${type}`
@@ -28,25 +45,23 @@ export async function fetchLog(type) {
     }
     const { data } = await api.get(path)
 
-    if (data.length > 0) {
+    if (Array.isArray(data) && data.length > 0) {
       lastIds[type] = data[0]['uid']
-
-      for (let i = data.length - 1; i >= 0; i -= 1) {
-        const { category, completed, level, message, time, uid } = data[i]
-        eventEmitter.emit('log', {
-          id: uid,
-          d: new Date(time),
-          subject: typeLabel[type],
-          text: `Completed: ${completed}
-Level: ${level}
-Category: ${category}
-Message:
-${message}`
-        })
-      }
+      emitLog(data, type)
       eventEmitter.emit('log.open')
 
       if (data.filter(item => item.completed).length === 0) {
+        setTimeout(() => fetchLog(type), 2000)
+      }
+    } else if (typeof data === 'object') {
+      let records = null
+      Object.keys(data).forEach(k => {
+        lastIds[type] = data[k][0]['uid']
+        records = data[k]
+        emitLog(data[k], type)
+      })
+
+      if (records.filter(item => item.completed).length === 0) {
         setTimeout(() => fetchLog(type), 2000)
       }
     }
