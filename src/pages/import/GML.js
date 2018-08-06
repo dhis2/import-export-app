2dcd0e63f22bfa87306d07c0fa0659f7c4a6b0e5
Muhammad Adeel <imadeelshahid@gmail.com>
2018-08-06 14:29:35 +0200

    add support for classKey to GMLImport

--- src/pages/import/GML.js
+++ src/pages/import/GML.js
@@ -3,8 +3,15 @@ import i18n from '@dhis2/d2-i18n'
 import { apiConfig } from 'config'
 import { eventEmitter } from 'services'
 import { FormBase } from 'components/FormBase'
-import { CTX_DEFAULT, TYPE_FILE, TYPE_RADIO } from 'components/Form'
+import {
+  CTX_DEFAULT,
+  CTX_CSV_OPTION,
+  TYPE_FILE,
+  TYPE_RADIO,
+  TYPE_SELECT
+} from 'components/Form'
 import { GMLIcon } from 'components/Icon'
+import { api } from 'services'
 import { fetchLog, getMimeType } from './helpers'
 
 export class GMLImport extends FormBase {
@@ -32,6 +39,18 @@ export class GMLImport extends FormBase {
     {
       context: CTX_DEFAULT,
       type: TYPE_RADIO,
+      name: 'format',
+      label: i18n.t('Format')
+    },
+    {
+      context: CTX_CSV_OPTION,
+      type: TYPE_SELECT,
+      name: 'classKey',
+      label: i18n.t('Class Key')
+    },
+    {
+      context: CTX_DEFAULT,
+      type: TYPE_RADIO,
       name: 'dryRun',
       label: i18n.t('Dry run')
     }
@@ -42,6 +61,29 @@ export class GMLImport extends FormBase {
 
     upload: {
       selected: null
+    },
+
+    format: {
+      selected: 'json',
+      values: [
+        {
+          value: 'json',
+          label: i18n.t('JSON')
+        },
+        {
+          value: 'xml',
+          label: i18n.t('XML')
+        },
+        {
+          value: 'csv',
+          label: i18n.t('CSV')
+        }
+      ]
+    },
+
+    classKey: {
+      selected: '',
+      values: []
     },
 
     dryRun: {
@@ -59,18 +101,58 @@ export class GMLImport extends FormBase {
     }
   }
 
+  async componentDidMount() {
+    await fetchLog('GML_IMPORT')
+    await this.fetch()
+  }
+
+  async fetch() {
+    try {
+      const { data } = await api.get('metadata/csvImportClasses')
+      const values = data.map(v => ({
+        value: v,
+        label: v.split('_').join(' ')
+      }))
+
+      this.setState({
+        classKey: {
+          values,
+          selected: values[0]['value']
+        }
+      })
+    } catch (e) {
+      console.log('fetch csvImportClasses failed')
+      console.log(e)
+    }
+  }
+
+  onFormUpdate = (name, value) => {
+    if (name === 'format') {
+      const { _context } = this.state
+
+      if (value === 'csv' && _context !== CTX_CSV_OPTION) {
+        this.changeContext(CTX_CSV_OPTION)
+      } else {
+        this.changeContext(CTX_DEFAULT)
+      }
+    }
+  }
+
   onSubmit = async () => {
     try {
-      const { upload, dryRun } = this.getFormState()
+      const { upload, format, classKey, dryRun } = this.getFormState()
 
       const formData = new FormData()
       formData.set('upload', upload)
 
-      // const extension = upload.name.substr(upload.name.lastIndexOf('.') + 1)
       const contentType = getMimeType(upload.name)
 
       const params = []
       params.push(`dryRun=${dryRun}`)
+
+      if (format === 'csv') {
+        params.push(`classKey=${classKey}`)
+      }
 
       eventEmitter.emit('log.open')
       this.setState({ processing: true })
