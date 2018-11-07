fae56b613f9740c460b9b3cdea1d590149ca5f4b
Muhammad Adeel <adeel@dhis2.org>
2018-11-07 11:50:03 +0100

    feat: support zipped imports

--- src/pages/import/helpers.js
+++ src/pages/import/helpers.js
@@ -9,13 +9,13 @@ const CATEGORY_2_LABEL = {
 }
 
 export function getMimeType(filename) {
-    if (filename.endsWith('json')) {
+    if (filename.endsWith('json') || filename.includes('.json')) {
         return 'application/json'
-    } else if (filename.endsWith('xml')) {
+    } else if (filename.endsWith('xml') || filename.includes('.xml')) {
         return 'application/xml'
-    } else if (filename.endsWith('csv')) {
+    } else if (filename.endsWith('csv') || filename.includes('.csv')) {
         return 'application/csv'
-    } else if (filename.endsWith('gml')) {
+    } else if (filename.endsWith('gml') || filename.includes('.gml')) {
         return 'application/xml'
     }
 
