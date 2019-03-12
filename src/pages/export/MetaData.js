becd9cdf86aa99cbb1fca8ae53c070fe1c109002
Birk Johansson <birk@dhis2.org>
2019-03-12 17:16:41 +0100

    fix(metadata-export): add support for csv. Download directly from server

--- src/pages/export/MetaData.js
+++ src/pages/export/MetaData.js
@@ -55,7 +55,7 @@ export class MetaDataExport extends FormBase {
 
     state = getFormValues([
         'schemas',
-        'format:.json:json,xml',
+        'format:.json:json,xml,csv',
         'compression',
         'sharing',
     ])
@@ -69,11 +69,10 @@ export class MetaDataExport extends FormBase {
                 sharing,
             } = this.getFormState()
 
-            const ext = format.substr(1)
-
             const params = []
             params.push('assumeTrue=false')
             params.push(`format=json`)
+            params.push('download=true')
             params.push(
                 schemas
                     .map(name => name)
@@ -90,25 +89,10 @@ export class MetaDataExport extends FormBase {
                 params.push('skipSharing=true')
             }
 
-            let endpoint = `metadata${format}`
-            if (compression !== 'none') {
-                endpoint += compression
-                window.location = api.url(`${endpoint}?${params.join('&')}`)
-                return
-            }
-
-            this.setMetaState({ processing: true }, async () => {
-                const { data } = await api.get(
-                    `${endpoint}?${params.join('&')}`
-                )
-                let contents = data
-                if (ext === 'json') {
-                    contents = JSON.stringify(data)
-                }
-
-                downloadBlob(createBlob(contents, ext), endpoint)
-                this.setState({ processing: false })
-            })
+            let endpoint = `metadata${format}${
+                compression !== 'none' ? compression : ''
+            }`
+            window.location = api.url(`${endpoint}?${params.join('&')}`)
         } catch (e) {
             console.log('MetaData Export error', e, '\n')
         }
