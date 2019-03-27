5ddc01f624634704acff3ca6a327c2fceb0f478c
Birk Johansson <birk@dhis2.org>
2019-03-27 14:40:16 +0100

    feat(metadata dependency): add support for optionSet

--- src/helpers/values.js
+++ src/helpers/values.js
@@ -258,6 +258,7 @@ const values = {
             ['categoryCombos', i18n.t('Category combination')],
             ['dashboards', i18n.t('Dashboard')],
             ['dataElementGroups', i18n.t('Data element groups')],
+            ['optionSet', i18n.t('Option set')],
         ]),
     },
 
