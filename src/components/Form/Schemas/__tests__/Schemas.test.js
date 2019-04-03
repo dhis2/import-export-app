01cbdbe58c21afcd3fad0a8960eead7e47c10e9e
Birk Johansson <birk@dhis2.org>
2019-04-03 14:42:30 +0200

    fix: failing test. Update UI dep

--- src/components/Form/Schemas/__tests__/Schemas.test.js
+++ src/components/Form/Schemas/__tests__/Schemas.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { mount } from 'enzyme'
+import { Provider } from 'react-redux'
 import Schemas from '../'
 import { Loading } from '../../../Loading'
 
@@ -7,14 +8,24 @@ import { MuiThemeProvider } from 'material-ui'
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 
+import { store } from 'store'
+
 const muiTheme = getMuiTheme(lightBaseTheme)
+
+const Wrapper = props => (
+    <Provider store={store}>
+        <MuiThemeProvider muiTheme={muiTheme}>
+            {props.children}
+        </MuiThemeProvider>
+    </Provider>
+)
 
 describe('Schemas', () => {
     it('empty render', () => {
         const wrapper = mount(
-            <MuiThemeProvider muiTheme={muiTheme}>
-                <Schemas name="schema" label="Schema" />
-            </MuiThemeProvider>
+            <Wrapper>
+                <Schemas name="schema" label="Schema" store={store} />
+            </Wrapper>
         )
 
         expect(wrapper.containsAllMatchingElements([<Loading />])).toEqual(true)
