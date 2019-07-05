7a049eac8a29df0f3c67c769c38298fb0358001e
Jan-Gerke Salomon <Mohammer5@users.noreply.github.com>
2019-07-05 06:58:23 +0200

    feat: add firstRowIsHeader param for csv meta data import (#43)

--- src/pages/import/MetaData.js
+++ src/pages/import/MetaData.js
@@ -34,6 +34,7 @@ export class MetaDataImport extends FormBase {
             'upload',
             'format',
             'dryRun',
+            'firstRowIsHeader',
             'classKey',
             'importMode',
             'identifier',
@@ -73,6 +74,7 @@ export class MetaDataImport extends FormBase {
         'skipValidation',
         'async',
         'inclusionStrategy',
+        'firstRowIsHeader',
     ])
 
     async componentDidMount() {
@@ -115,8 +117,15 @@ export class MetaDataImport extends FormBase {
 
     onSubmit = async () => {
         try {
-            const { upload, format, classKey } = this.getFormState()
+            let append = []
             const formData = new FormData()
+            const {
+                upload,
+                format,
+                classKey,
+                firstRowIsHeader,
+            } = this.getFormState()
+
             if (!upload) {
                 this.assertOnError({
                     target: {
@@ -126,9 +135,17 @@ export class MetaDataImport extends FormBase {
                     },
                 })
             }
+
             formData.set('upload', upload)
 
-            const append = format === '.csv' ? [`classKey=${classKey}`] : []
+            if (format === '.csv') {
+                append = [
+                    ...append,
+                    `classKey=${classKey}`,
+                    `firstRowIsHeader=${firstRowIsHeader}`,
+                ]
+            }
+
             append.push('format=json')
 
             const params = getParamsFromFormState(
