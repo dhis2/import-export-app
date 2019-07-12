8d99490b026dac409c0ba2716d7263e6d643a614
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-07-12 08:29:06 +0200

    fix: add missing Content-Type for pdf

--- src/helpers/mime.js
+++ src/helpers/mime.js
@@ -4,6 +4,7 @@ const mapping = {
     csv: 'application/csv',
     gml: 'application/xml',
     adx: 'application/adx+xml',
+    pdf: 'application/pdf',
 }
 
 export const getMimeType = format => mapping[format] || null
