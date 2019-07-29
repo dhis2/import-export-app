03a240a2a87c2a52df93530852d2b022f475a156
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-07-29 16:17:03 +0200

    fix: remove text-transform uppercase from radio labels

--- src/components/Inputs/Format.js
+++ src/components/Inputs/Format.js
@@ -2,11 +2,11 @@ import i18n from '@dhis2/d2-i18n'
 import React from 'react'
 import { RadioGroup } from '../FinalFormComponents/RadioGroup'
 
-export const OPTION_JSON = { value: 'json', label: i18n.t('json') }
-export const OPTION_XML = { value: 'xml', label: i18n.t('xml') }
-export const OPTION_PDF = { value: 'pdf', label: i18n.t('pdf') }
-export const OPTION_ADX = { value: 'adx', label: i18n.t('adx') }
-export const OPTION_CSV = { value: 'csv', label: i18n.t('csv') }
+export const OPTION_JSON = { value: 'json', label: i18n.t('JSON') }
+export const OPTION_XML = { value: 'xml', label: i18n.t('XML') }
+export const OPTION_PDF = { value: 'pdf', label: i18n.t('PDF') }
+export const OPTION_ADX = { value: 'adx', label: i18n.t('ADX') }
+export const OPTION_CSV = { value: 'csv', label: i18n.t('CSV') }
 export const FORMAT_KEY = 'format'
 export const FORMAT_DEFAULT_VALUE = OPTION_JSON.value
 
