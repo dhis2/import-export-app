02b4b32ecc5de4c775aa988597698eebf7eeee0f
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-08-05 17:13:27 +0200

    test(more options): check if className show is set for both scenarios

--- src/components/FormSections/__test__/MoreOptions.test.js
+++ src/components/FormSections/__test__/MoreOptions.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
+import { mount } from 'enzyme'
 import { MuiThemeProvider } from 'material-ui'
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
@@ -9,34 +9,32 @@ describe('Input component - MoreOptions', () => {
     const muiTheme = getMuiTheme(lightBaseTheme)
 
     it('should render correctly & hide content', () => {
-        const file = renderer
-            .create(
-                <MuiThemeProvider muiTheme={muiTheme}>
-                    <MoreOptions>
-                        <span>Foo</span>
-                        <span>Bar</span>
-                        <span>Baz</span>
-                    </MoreOptions>
-                </MuiThemeProvider>
-            )
-            .toJSON()
+        const file = mount(
+            <MuiThemeProvider muiTheme={muiTheme}>
+                <MoreOptions>
+                    <span>Foo</span>
+                    <span>Bar</span>
+                    <span>Baz</span>
+                </MoreOptions>
+            </MuiThemeProvider>
+        )
 
-        expect(file).toMatchSnapshot()
+        const content = file.find('.content')
+        expect(content.get(0).props.className).not.toMatch(/show/)
     })
 
     it('should render correctly & show content', () => {
-        const file = renderer
-            .create(
-                <MuiThemeProvider muiTheme={muiTheme}>
-                    <MoreOptions openInitially>
-                        <span>Foo</span>
-                        <span>Bar</span>
-                        <span>Baz</span>
-                    </MoreOptions>
-                </MuiThemeProvider>
-            )
-            .toJSON()
+        const file = mount(
+            <MuiThemeProvider muiTheme={muiTheme}>
+                <MoreOptions openInitially>
+                    <span>Foo</span>
+                    <span>Bar</span>
+                    <span>Baz</span>
+                </MoreOptions>
+            </MuiThemeProvider>
+        )
 
-        expect(file).toMatchSnapshot()
+        const content = file.find('.content')
+        expect(content.get(0).props.className).toMatch(/show/)
     })
 })
