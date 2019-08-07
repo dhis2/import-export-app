87ce8d6ba1727a3b36abfb3a005c865286d1b17e
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-08-07 09:20:30 +0200

    test: adjust useField mock to accept args

--- src/__mocks__/react-final-form.js
+++ src/__mocks__/react-final-form.js
@@ -1,11 +1,11 @@
 import React, { Fragment } from 'react'
 
-export const Form = ({ children }) => <Fragment>{children}</Fragment>
+export const Form = ({ children }) => <Fragment>{children()}</Fragment>
 
-export const useField = jest.fn(() => ({
+export const useField = jest.fn((name, { value }) => ({
     input: {
-        name: 'Name',
-        value: '',
+        name,
+        value,
         onChange: jest.fn(),
     },
     meta: {
