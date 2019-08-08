eac9b1de2f00904d4ca2f62015737779a1f93954
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-08-08 15:07:50 +0200

    fix(import strategy): use correct value for create & update

--- src/components/Inputs/ImportStrategy.js
+++ src/components/Inputs/ImportStrategy.js
@@ -2,8 +2,8 @@ import i18n from '@dhis2/d2-i18n'
 import React from 'react'
 import { RadioGroup } from '../FinalFormComponents/RadioGroup'
 
-export const OPTION_CREATE_AND_UPDATES = {
-    value: 'CREATE_AND_UPDATES',
+export const OPTION_CREATE_AND_UPDATE = {
+    value: 'CREATE_AND_UPDATE',
     label: i18n.t('New and updates'),
 }
 export const OPTION_CREATE = { value: 'CREATE', label: i18n.t('New only') }
@@ -13,14 +13,14 @@ export const OPTION_UPDATES = {
 }
 export const OPTION_DELETE = { value: 'DELETE', label: i18n.t('Delete') }
 export const IMPORT_STRATEGY_KEY = 'importStrategy'
-export const IMPORT_STRATEGY_DEFAULT_VALUE = OPTION_CREATE_AND_UPDATES.value
+export const IMPORT_STRATEGY_DEFAULT_VALUE = OPTION_CREATE_AND_UPDATE.value
 
 export const ImportStrategy = () => (
     <RadioGroup
         name={IMPORT_STRATEGY_KEY}
         label={i18n.t('Import strategy')}
         options={[
-            OPTION_CREATE_AND_UPDATES,
+            OPTION_CREATE_AND_UPDATE,
             OPTION_CREATE,
             OPTION_UPDATES,
             OPTION_DELETE,
