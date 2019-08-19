9176919093f36af710c225e393afddf98d577de8
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-08-19 17:31:07 +0200

    refactor(event import): use final-form & new components

---
+++ src/components/Inputs/EventIdScheme.js
@@ -0,0 +1,17 @@
+import i18n from '@dhis2/d2-i18n'
+import React from 'react'
+import { RadioGroup } from '../FinalFormComponents/RadioGroup'
+
+export const OPTION_UID = { value: 'UID', label: i18n.t('Uid') }
+export const OPTION_CODE = { value: 'CODE', label: i18n.t('Code') }
+
+export const EVENT_ID_SCHEME_KEY = 'eventIdScheme'
+export const EVENT_ID_SCHEME_DEFAULT_VALUE = OPTION_UID.value
+
+export const EventIdScheme = () => (
+    <RadioGroup
+        name={EVENT_ID_SCHEME_KEY}
+        label={i18n.t('Event id scheme')}
+        options={[OPTION_UID, OPTION_CODE]}
+    />
+)
