e16ce27fcc9503feb07aee9d02d3934cf65139c0
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-12-19 18:08:17 +0800

    feat(attributes): prepare attributes fetching & state management
    * setup d2 api & add attribute fetching helper
    * add redux components to handle & store attributes
    * load & display dynamic data element and org unit id scheme options
    * simplify data export dynamic options

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,12 @@
 import { combineReducers } from 'redux'
 
 import user from './user'
+import attributes from './attributes'
 
 export * from 'reducers/user/actions'
+export * from './attributes/actions'
 
 export default combineReducers({
     user,
+    attributes,
 })
