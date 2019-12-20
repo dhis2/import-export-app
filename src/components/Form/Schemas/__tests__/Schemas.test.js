337f966e91a5c07d64056de6a810113daba4e4f2
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-12-20 09:41:44 +0800

    chore: fix tests

--- src/components/Form/Schemas/__tests__/Schemas.test.js
+++ src/components/Form/Schemas/__tests__/Schemas.test.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import { mount } from 'enzyme'
 import { Provider } from 'react-redux'
-import Schemas from '../'
-import { Loading } from '../../../Loading'
-
 import { MuiThemeProvider } from 'material-ui'
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 
-import { store } from 'store'
+import Schemas from '../'
+import { Loading } from '../../../Loading'
+import { configureStore } from '../../../../configureStore'
+
+jest.mock('../../../../configureStore')
 
 const muiTheme = getMuiTheme(lightBaseTheme)
-
+const store = configureStore()
 const Wrapper = props => (
     <Provider store={store}>
         <MuiThemeProvider muiTheme={muiTheme}>
