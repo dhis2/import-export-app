a179c330c70f0076029283740c166a768bbff83f
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-12-20 09:16:51 +0800

    feat(dynamic attr options): add on data export and import pages
    Fixes DHIS2-6991

--- src/pages/import/Data.js
+++ src/pages/import/Data.js
@@ -1,3 +1,4 @@
+import { connect } from 'react-redux'
 import React from 'react'
 import i18n from '@dhis2/d2-i18n'
 import { apiConfig } from 'config'
@@ -9,10 +10,21 @@ import {
     getFormValues,
     getParamsFromFormState,
     getUploadXHR,
+    values,
 } from 'helpers'
 import { fetchLog } from './helpers'
 
-export class DataImport extends FormBase {
+import {
+    fetchUniqueDataElementAttributes,
+    fetchUniqueOrgUnitAttributes,
+} from '../../reducers/attributes/thunks'
+import {
+    getSharedAttributes,
+    getSharedAttributesLoading,
+} from '../../reducers/attributes/selectors'
+import s from '../../components/Form/styles.css'
+
+class DataImport extends FormBase {
     static path = '/import/data'
 
     static order = 2
@@ -60,7 +72,74 @@ export class DataImport extends FormBase {
     ])
 
     async componentDidMount() {
+        // creating default props here because componentDidUpdate
+        // will not be called on initial render
+        this.computeFieldOverrideValues({
+            dataElementAttributes: [],
+            orgUnitAttributes: [],
+        })
+        this.props.fetchDataElementAttributes()
+        this.props.fetchOrganisationUnitAttributes()
         await fetchLog('', 'DATAVALUE_IMPORT')
+    }
+
+    componentDidUpdate(prevProps) {
+        this.computeFieldOverrideValues(prevProps)
+    }
+
+    computeFieldOverrideValues(prevProps) {
+        // Only set field overrides if the amount of elements changed in the store
+        // These values will be loaded on page load only anyways
+        if (
+            prevProps.dataElementAttributes.length !==
+                this.props.dataElementAttributes.length ||
+            prevProps.orgUnitAttributes.length !==
+                this.props.orgUnitAttributes.length
+        ) {
+            // Collect default form options and add dynamic ones
+            const dataElementIdScheme = [
+                ...values.dataElementIdScheme.values,
+                ...this.props.dataElementAttributes.map(
+                    ({ id, displayName: label }) => ({
+                        value: `ATTRIBUTE:${id}`,
+                        label,
+                    })
+                ),
+            ]
+
+            const orgUnitIdScheme = [
+                ...values.orgUnitIdScheme.values,
+                ...this.props.orgUnitAttributes.map(
+                    ({ id, displayName: label }) => ({
+                        value: `ATTRIBUTE:${id}`,
+                        label,
+                    })
+                ),
+            ]
+
+            const idScheme = [
+                ...values.idScheme.values,
+                ...this.props.sharedAttributes.map(
+                    ({ id, displayName: label }) => ({
+                        value: `ATTRIBUTE:${id}`,
+                        label,
+                    })
+                ),
+            ]
+
+            // Set the override values
+            // These will be used by the Form copmonent
+            // to build the input components
+            // NOTE: this.forceUpdate() wouldn't be needed
+            // if "idScheme" was hidden before clicking on "more options"
+            // Due to that bug.. We have to force an update after this
+            this.fieldValuesOverride = {
+                dataElementIdScheme,
+                orgUnitIdScheme,
+                idScheme,
+            }
+            this.forceUpdate()
+        }
     }
 
     onSubmit = () => {
@@ -104,4 +183,45 @@ export class DataImport extends FormBase {
             console.log('Data Import error', e, '\n')
         }
     }
+
+    render() {
+        const form = super.render()
+
+        if (this.props.loadingAttributes) {
+            return (
+                <div className={s.wrapper}>
+                    <div
+                        className={s.form}
+                        style={{
+                            padding: '14px 20px',
+                            width: 800,
+                            boxSizing: 'border-box',
+                            margin: '60px auto',
+                        }}
+                    >
+                        {i18n.t('Loading options...')}
+                    </div>
+                </div>
+            )
+        }
+
+        return form
+    }
 }
+
+const ConnectedDataImport = connect(
+    state => ({
+        loadingAttributes: getSharedAttributesLoading(state),
+        dataElementAttributes: state.attributes.dataElement.data,
+        orgUnitAttributes: state.attributes.organisationUnit.data,
+        sharedAttributes: getSharedAttributes(state),
+    }),
+    dispatch => ({
+        fetchDataElementAttributes: () =>
+            dispatch(fetchUniqueDataElementAttributes()),
+        fetchOrganisationUnitAttributes: () =>
+            dispatch(fetchUniqueOrgUnitAttributes()),
+    })
+)(DataImport)
+
+export { ConnectedDataImport as DataImport }
