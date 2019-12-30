c0cd92f75421ce27ec1da46997d72ae349cc2a97
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-12-30 16:44:23 +0800

    feat(input components): add missing components for Event export page

---
+++ src/components/Inputs/ProgramStages.js
@@ -0,0 +1,91 @@
+import { FormSpy, useForm } from 'react-final-form'
+import { useDispatch, useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import i18n from '@dhis2/d2-i18n'
+
+import { Field } from '../Field/Field'
+import { Label } from '../Field/Label'
+import { Select } from '../FinalFormComponents/Select'
+import { fetchProgramStages } from '../../reducers/programStage/thunks'
+import {
+    getProgramStages,
+    getProgramStagesError,
+    getProgramStagesLoading,
+} from '../../reducers/programStage/selectors'
+
+export const OPTION_SELECT_STAGE = {
+    value: '',
+    label: i18n.t('[ All program stages]'),
+}
+export const PROGRAM_STAGES_DEFAULT_OPTIONS = [OPTION_SELECT_STAGE]
+export const PROGRAM_STAGES_KEY = 'programStages'
+export const PROGRAM_STAGES_DEFAULT_VALUE = OPTION_SELECT_STAGE.value
+
+const programStagesLabel = i18n.t('Program stages')
+
+const RenderComponent = ({ values }) => {
+    const form = useForm()
+    const dispatch = useDispatch()
+    const program = values ? values.program : null
+
+    useEffect(() => {
+        if (program) {
+            dispatch(fetchProgramStages(program))
+            form.change(PROGRAM_STAGES_KEY, '')
+        }
+    }, [program]) // eslint-disable-line react-hooks/exhaustive-deps
+
+    return null
+}
+
+export const ProgramStages = () => {
+    let component
+    const programStages = useSelector(getProgramStages)
+    const loading = useSelector(getProgramStagesLoading)
+    const error = useSelector(getProgramStagesError)
+
+    if (loading) {
+        component = <ProgramStagesLoading />
+    } else if (error) {
+        component = <ProgramStagesError error={error} />
+    } else {
+        const options = [...PROGRAM_STAGES_DEFAULT_OPTIONS, ...programStages]
+        component = (
+            <Field>
+                <Select
+                    name={PROGRAM_STAGES_KEY}
+                    label={programStagesLabel}
+                    options={options}
+                    dataTest="input-programStages"
+                />
+            </Field>
+        )
+    }
+
+    return (
+        <>
+            <FormSpy
+                subscription={{ values: { program: true } }}
+                render={({ values }) => <RenderComponent values={values} />}
+            />
+
+            {component}
+        </>
+    )
+}
+
+export const ProgramStagesLoading = () => (
+    <Field>
+        <Label>{programStagesLabel}</Label>
+        {i18n.t('Loading programStage options...')}
+    </Field>
+)
+
+export const ProgramStagesError = ({ error }) => (
+    <Field>
+        <Label>{programStagesLabel}</Label>
+        {i18n.t('Something went wrong when loading the programStages!')}
+        <br />
+        {error}
+    </Field>
+)
---
+++ src/components/Inputs/Programs.js
@@ -0,0 +1,79 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { useForm } from 'react-final-form'
+import React, { useEffect } from 'react'
+import i18n from '@dhis2/d2-i18n'
+
+import { Field } from '../Field/Field'
+import { Label } from '../Field/Label'
+import { Select } from '../FinalFormComponents/Select'
+import { fetchPrograms } from '../../reducers/program/thunks'
+import {
+    getPrograms,
+    getProgramsError,
+    getProgramsLoaded,
+    getProgramsLoading,
+} from '../../reducers/program/selectors'
+
+export const PROGRAMS_KEY = 'program'
+export const PROGRAMS_DEFAULT_VALUE = ''
+
+const programsLabel = i18n.t('Programs')
+
+const useLoadPrograms = () => {
+    const form = useForm()
+    const dispatch = useDispatch()
+    const loading = useSelector(getProgramsLoading)
+    const loaded = useSelector(getProgramsLoaded)
+    const error = useSelector(getProgramsError)
+
+    useEffect(() => {
+        if (!loaded && !loading && !error) {
+            dispatch(fetchPrograms()).then(({ payload }) => {
+                const { programs } = payload
+
+                if (!programs.length) return
+
+                const [firstProgram] = payload.programs
+                form.change(PROGRAMS_KEY, firstProgram.value)
+            })
+        }
+    }, [loaded, loading, error, dispatch, form])
+}
+
+export const Programs = () => {
+    useLoadPrograms()
+
+    const programs = useSelector(getPrograms)
+    const loading = useSelector(getProgramsLoading)
+    const error = useSelector(getProgramsError)
+
+    if (loading) return <ProgramsLoading />
+    if (error) return <ProgramsError error={error} />
+
+    return (
+        <Field>
+            <Select
+                name={PROGRAMS_KEY}
+                label={programsLabel}
+                options={programs}
+                dataTest="input-programs"
+            />
+        </Field>
+    )
+}
+
+export const ProgramsLoading = () => (
+    <Field>
+        <Label>{programsLabel}</Label>
+        {i18n.t('Loading program options...')}
+    </Field>
+)
+
+export const ProgramsError = ({ error }) => (
+    <Field>
+        <Label>{programsLabel}</Label>
+        {i18n.t('Something went wrong when loading the programs!')}
+        <br />
+        {error}
+    </Field>
+)
