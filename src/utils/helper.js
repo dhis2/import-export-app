260c871a470390e0d37980421555445750745085
Wahed Mangal <mangal.wahed@gmail.com>
2020-01-17 15:51:01 +0100

    chore: move attribute fetcher function to common utils file

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,3 +1,5 @@
+import i18n from '@dhis2/d2-i18n';
+
 const pathToId = path => {
     const pathSplit = path.split('/');
     const orgId = pathSplit[pathSplit.length - 1];
@@ -43,4 +45,79 @@ const downloadBlob = (url, filename) => {
     link.remove();
 };
 
-export { createBlob, downloadBlob, jsDateToISO8601, pathToId };
+const fetchAndSetAttributes = (
+    apiBaseUrl,
+    setDataElementIdSchemeOptions,
+    setOrgUnitIdSchemeOptions,
+    setIdSchemeOptions
+) => {
+    const fetchOptions = {
+        credentials: 'include',
+    };
+
+    const fetcher = (url, succF, errF) =>
+        fetch(url, fetchOptions)
+            .then(resp => resp.json())
+            .then(json => {
+                succF(json);
+            })
+            .catch(e => {
+                errF(e);
+            });
+
+    const fields = 'id,displayName';
+    const filters = attr => `unique:eq:true&filter=${attr}:eq:true`;
+    const dataElAttributesURL = `${apiBaseUrl}attributes.json?paging=false&fields=${fields}&filter=${filters(
+        'dataElementAttribute'
+    )}`;
+    const orgUnitAttributesURL = `${apiBaseUrl}attributes.json?paging=false&fields=${fields}&filter=${filters(
+        'organisationUnitAttribute'
+    )}`;
+
+    fetcher(
+        dataElAttributesURL,
+        ({ attributes }) => {
+            setDataElementIdSchemeOptions(old => {
+                return [
+                    ...old,
+                    ...attributes.map(({ id, displayName }) => ({
+                        value: `ATTRIBUTE:${id}`,
+                        label: i18n.t(displayName),
+                    })),
+                ];
+            });
+        },
+        e => {
+            console.log('DataExport data element attributes fetch error: ', e);
+        }
+    );
+
+    fetcher(
+        orgUnitAttributesURL,
+        ({ attributes }) => {
+            setOrgUnitIdSchemeOptions(old => {
+                return [
+                    ...old,
+                    ...attributes.map(({ id, displayName }) => ({
+                        value: `ATTRIBUTE:${id}`,
+                        label: i18n.t(displayName),
+                    })),
+                ];
+            });
+        },
+        e => {
+            console.log(
+                'DataExport organization unit attributes fetch error: ',
+                e
+            );
+        }
+    );
+};
+
+export {
+    createBlob,
+    downloadBlob,
+    fetchAndSetAttributes,
+    jsDateToISO8601,
+    pathToId,
+};
