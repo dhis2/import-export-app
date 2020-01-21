e37ca1cbbb560a1c6d5908c508d3c81e046eb220
Wahed Mangal <mangal.wahed@gmail.com>
2020-01-21 10:43:37 +0100

    chore: form field now takes a _required_ prop

--- src/components/FormField/FormField.js
+++ src/components/FormField/FormField.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 import s from './FormField.module.css';
 
-const FormField = ({ label, children }) => {
+const FormField = ({ label, required = false, children }) => {
     return (
         <div className={s.container}>
-            <span className={s.label}>{label}</span>
+            <span className={s.label}>
+                {label}
+                {required && <span> *</span>}
+            </span>
             {children}
         </div>
     );
