9d589baa670c8c36ee8f74af59f018225ba7ff5a
Wahed Mangal <mangal.wahed@gmail.com>
2020-01-21 10:45:39 +0100

    chore: now takes an optional _help_ prop

--- src/components/Switch/Switch.js
+++ src/components/Switch/Switch.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { Switch as UISwitch } from '@dhis2/ui-core';
+import { Help, Switch as UISwitch } from '@dhis2/ui-core';
 
 import { FormField } from '../FormField';
 
-const Switch = ({ name, label, checked, setChecked }) => {
+const Switch = ({ name, label, help, checked, setChecked }) => {
     return (
         <FormField label={label}>
-            <UISwitch
-                name={name}
-                onChange={() => setChecked(!checked)}
-                checked={checked}
-            />
+            <>
+                <UISwitch
+                    name={name}
+                    onChange={() => setChecked(!checked)}
+                    checked={checked}
+                />
+                {help && <Help>{help}</Help>}
+            </>
         </FormField>
     );
 };
