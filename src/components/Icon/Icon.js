340114b615100b2537c64b193e2e08c5fa3fd288
Wahed Mangal <mangal.wahed@gmail.com>
2020-01-21 10:44:34 +0100

    feat: upload icon

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -73,3 +73,10 @@ export const MinusIcon = props => (
         <path d="M0 0h24v24H0z" fill="none" />
     </Icon>
 );
+
+export const UploadIcon = props => (
+    <Icon {...props}>
+        <path d="M0 0h24v24H0z" fill="none" />
+        <path d="M9 16h6v-6h4l-7-7-7 7h4zm-4 2h14v2H5z" />
+    </Icon>
+);
