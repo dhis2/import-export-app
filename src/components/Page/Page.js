2ec221afe027046db5c9438acf8bd1ceefb2ff4e
Wahed Mangal <mangal.wahed@gmail.com>
2020-01-22 10:59:46 +0100

    fix: page supports progress in percentage

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import { Card, CircularLoader, ComponentCover } from '@dhis2/ui-core';
+import {
+    Card,
+    CircularLoader,
+    ComponentCover,
+    LinearLoader,
+} from '@dhis2/ui-core';
 
 import s from './Page.module.css';
 
 const Page = ({ title, desc, icon, children, loading = false }) => {
+    const loadingEl =
+        typeof loading == 'number' ? (
+            <LinearLoader amount={loading} />
+        ) : (
+            <CircularLoader />
+        );
+
     return (
         <div className={s.container}>
             <div className={s.header}>
@@ -12,11 +24,7 @@ const Page = ({ title, desc, icon, children, loading = false }) => {
                 <p className={s.desc}>{desc}</p>
             </div>
             <Card>
-                {loading && (
-                    <ComponentCover>
-                        <CircularLoader />
-                    </ComponentCover>
-                )}
+                {!!loading && <ComponentCover>{loadingEl}</ComponentCover>}
                 <div className={s.content}>{children}</div>
             </Card>
         </div>
