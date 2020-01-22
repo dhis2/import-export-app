a075303da3fbc91e463732039eefa89e884cdcf0
Wahed Mangal <mangal.wahed@gmail.com>
2020-01-22 11:38:51 +0100

    fix: state updates on unmounted component

--- src/components/FormAlerts/FormAlerts.js
+++ src/components/FormAlerts/FormAlerts.js
@@ -4,19 +4,25 @@ import { AlertBar } from '@dhis2/ui-core';
 import s from './FormAlerts.module.css';
 
 const FormAlerts = ({ alerts }) => {
-    if (alerts.length == 0) return null;
+    const [bars, setBars] = useState([]);
 
-    const bars = alerts.map(a => (
-        <AlertBar
-            key={`alert-${a.id}`}
-            duration={a.duration ? a.duration : 8000}
-            warning={a.warning}
-            info={a.info}
-            critical={a.critical}
-        >
-            {a.message}
-        </AlertBar>
-    ));
+    useEffect(() => {
+        const newBars = alerts.map(a => (
+            <AlertBar
+                key={`alert-${a.id}`}
+                warning={a.warning}
+                info={a.info}
+                critical={a.critical}
+                success={a.success}
+                duration={8000}
+            >
+                {a.message}
+            </AlertBar>
+        ));
+        setBars(newBars);
+    }, [alerts]);
+
+    if (bars.length == 0) return null;
 
     return <div className={s.container}>{bars}</div>;
 };
