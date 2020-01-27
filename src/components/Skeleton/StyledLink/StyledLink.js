d1c609b71d41929e1b44a040a61d2f2f39588c73
Wahed Mangal <mangal.wahed@gmail.com>
2020-01-27 10:35:23 +0100

    chore: root path redirects to data-import page

--- src/components/Skeleton/StyledLink/StyledLink.js
+++ src/components/Skeleton/StyledLink/StyledLink.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
 import s from './StyledLink.module.css'
 
-const StyledLink = props => <Link className={s.link} {...props} />
+const StyledLink = ({ dataTest, ...passThroughProps }) => (
+    <Link data-test={dataTest} className={s.link} {...passThroughProps} />
+)
+
+StyledLink.propTypes = {
+    dataTest: PropTypes.string.isRequired,
+}
 
 export { StyledLink }
