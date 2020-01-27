abb4675bb27c3ee5126595ff6253ab581eb0b1d2
Wahed Mangal <mangal.wahed@gmail.com>
2020-01-27 16:05:31 +0100

    chore: helper function to find the most recently created task

--- src/contexts/Task.js
+++ src/contexts/Task.js
@@ -2,4 +2,9 @@ import React from 'react'
 
 const TaskContext = React.createContext(undefined)
 
-export { TaskContext }
+const getNewestTask = tasks =>
+    Object.keys(tasks)
+        .map(k => tasks[k])
+        .sort((a, b) => (a.created < b.created ? 1 : -1))[0]
+
+export { TaskContext, getNewestTask }
