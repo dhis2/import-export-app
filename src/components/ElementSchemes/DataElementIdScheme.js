bdc09d5fd838801e5a7b38f3e70b134c05e4ff2a
Wahed Mangal <mangal.wahed@gmail.com>
2020-02-03 12:32:57 +0100

    fix: move schema logic inside components

--- src/components/ElementSchemes/DataElementIdScheme.js
+++ src/components/ElementSchemes/DataElementIdScheme.js
@@ -1,22 +1,45 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
+import { useConfig } from '@dhis2/app-runtime'
 import PropTypes from 'prop-types'
 import i18n from '@dhis2/d2-i18n'
 
-import { optionPropType } from '../../utils/options'
+import { fetchAttributes } from '../../utils/helper'
+import { dataElementIdSchemeOptions, optionPropType } from '../../utils/options'
 import { SchemeContext } from '../../contexts/'
 import { Select } from '../Select'
 
 const DataElementIdScheme = ({ selected, setSelected, dataTest }) => {
-    const { DataElementId } = useContext(SchemeContext)
+    const { baseUrl } = useConfig()
+    const { DataElementId, updateSchema } = useContext(SchemeContext)
+
+    useEffect(() => {
+        const f = async () => {
+            const attributes = await fetchAttributes(
+                `${baseUrl}/api/`,
+                'dataElementAttribute'
+            )
+            updateSchema('DataElementId', {
+                options: attributes,
+                loaded: true,
+                error: false,
+            })
+        }
+        if (!DataElementId.loaded) {
+            f()
+        }
+    }, [])
+
+    const options = [...dataElementIdSchemeOptions, ...DataElementId.options]
     return (
         <Select
             name="dataElementIdScheme"
             label={i18n.t('Data element ID scheme')}
-            options={DataElementId}
+            options={options}
             selected={selected}
             setValue={setSelected}
-            dense
             dataTest={dataTest}
+            loading={!DataElementId.loaded}
+            dense
         />
     )
 }
