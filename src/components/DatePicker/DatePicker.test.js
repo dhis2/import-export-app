b7f267b8c50c748b2f6f112e737cda242b0b4e24
Wahed Mangal <mangal.wahed@gmail.com>
2020-02-10 14:55:05 +0100

    feat: component DatePicker tests

---
+++ src/components/DatePicker/DatePicker.test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { render } from 'test-utils'
+import '@testing-library/jest-dom/extend-expect'
+
+import { DatePicker } from '.'
+
+const props = {
+    dataTest: 'date-picker',
+    name: 'file',
+    label: 'Start date',
+    date: new Date('10 Feb 2020 00:00:00 GMT'),
+    onChange: () => 1,
+}
+
+it('matches snapshot', () => {
+    const { asFragment } = render(<DatePicker {...props} />)
+    expect(asFragment()).toMatchSnapshot()
+})
