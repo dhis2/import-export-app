611b80f19c2bfd848a0c56a3b4464bf0de873844
Wahed Mangal <mangal.wahed@gmail.com>
2020-02-13 13:24:42 +0100

    chore: re-generate snapshots with current jest config

--- src/components/JobOverview/__test__/MenuLabel.test.js
+++ src/components/JobOverview/__test__/MenuLabel.test.js
@@ -2,12 +2,8 @@ import React from 'react'
 import { render } from 'test-utils'
 import '@testing-library/jest-dom/extend-expect'
 
-import { tasksState } from './data'
+import { allJobs } from './data'
 import { MenuLabel } from '../MenuLabel'
-
-const allJobs = ['data', 'event', 'gml', 'metadata']
-    .map(type => Object.keys(tasksState[type]).map(id => tasksState[type][id]))
-    .flat()
 
 it('matches snapshot', () => {
     const { asFragment } = render(<MenuLabel task={allJobs[0]} />)
