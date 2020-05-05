70fee610cca758714886bf1cdac51f4c6eaa66cf
Jan-Gerke Salomon <Mohammer5@users.noreply.github.com>
2020-05-05 16:52:52 +0200

    feat(event import): add data-element id scheme & id scheme input fields (#546)

--- src/pages/import/Event.js
+++ src/pages/import/Event.js
@@ -1,10 +1,10 @@
 import { Button } from '@dhis2/ui-core'
 import { Form } from 'react-final-form'
-import { connect } from 'react-redux'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import cx from 'classnames'
 import i18n from '@dhis2/d2-i18n'
 
+import { DataElementIdScheme } from '../../components/Inputs/DataElementIdScheme'
 import { DryRun } from '../../components/Inputs/DryRun'
 import { Error } from '../../components/Error'
 import { EventIcon } from '../../components/Icon'
@@ -13,50 +13,22 @@ import { FormContent } from '../../components/FormSections/FormContent'
 import { FormFooter } from '../../components/FormSections/FormFooter'
 import { FormHeader } from '../../components/FormSections/FormHeader'
 import { Format } from '../../components/Inputs/Format'
-import {
-    ORG_UNIT_ID_SCHEME_DEFAULT_OPTIONS,
-    OrgUnitIdSchemeLoading,
-    OrgUnitIdScheme,
-} from '../../components/Inputs/OrgUnitIdScheme'
+import { IdScheme } from '../../components/Inputs/idScheme'
+import { OrgUnitIdScheme } from '../../components/Inputs/OrgUnitIdScheme'
 import { Progress } from '../../components/Loading/Progress'
 import { TaskSummary } from '../../components/TaskSummary'
 import { Upload } from '../../components/Inputs/Upload'
-import { fetchUniqueOrgUnitAttributes } from '../../reducers/attributes/thunks'
 import { supportedFormats, defaultValues, onSubmit } from './Event/helper'
 import stylesForm from '../../components/Form/styles.module.css'
 import stylesFormBase from '../../components/FormBase/styles.module.css'
 
-const EventImport = ({
-    orgUnitAttributes,
-    orgUnitAttributesLoaded,
-    loadingOrgUnitAttributes,
-    fetchOrganisationUnitAttributes,
-}) => {
-    useEffect(
-        () => {
-            if (!orgUnitAttributesLoaded) {
-                fetchOrganisationUnitAttributes()
-            }
-        },
-
-        // load attributes on componentDidMount
-        [] // eslint-disable-line react-hooks/exhaustive-deps
-    )
-
+export const EventImport = () => {
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState('')
     const onSubmitHandler = onSubmit(setLoading, setError)
 
     if (error) return <Error message={error} onClear={() => setError('')} />
     if (loading) return <Progress />
-
-    const orgUnitIdSchemeOptions = [
-        ...ORG_UNIT_ID_SCHEME_DEFAULT_OPTIONS,
-        ...orgUnitAttributes.map(({ id, displayName: label }) => ({
-            value: `ATTRIBUTE:${id}`,
-            label,
-        })),
-    ]
 
     return (
         <Form onSubmit={onSubmitHandler} initialValues={defaultValues}>
@@ -77,15 +49,11 @@ const EventImport = ({
                             <Upload />
                             <Format options={supportedFormats} />
                             <DryRun />
-                            <EventIdScheme />
 
-                            {loadingOrgUnitAttributes ? (
-                                <OrgUnitIdSchemeLoading />
-                            ) : (
-                                <OrgUnitIdScheme
-                                    options={orgUnitIdSchemeOptions}
-                                />
-                            )}
+                            <EventIdScheme />
+                            <DataElementIdScheme />
+                            <OrgUnitIdScheme />
+                            <IdScheme />
                         </FormContent>
 
                         <FormFooter>
@@ -107,17 +75,3 @@ EventImport.desc = i18n.t(
 )
 
 EventImport.menuIcon = <EventIcon />
-
-const ConnectedEventImport = connect(
-    state => ({
-        orgUnitAttributes: state.attributes.organisationUnit.data,
-        orgUnitAttributesLoaded: state.attributes.organisationUnit.loaded,
-        loadingOrgUnitAttributes: state.attributes.organisationUnit.loading,
-    }),
-    dispatch => ({
-        fetchOrganisationUnitAttributes: () =>
-            dispatch(fetchUniqueOrgUnitAttributes()),
-    })
-)(EventImport)
-
-export { ConnectedEventImport as EventImport }
