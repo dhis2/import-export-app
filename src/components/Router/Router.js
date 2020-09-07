5f63c4d1dd54f2932b9a86a43ab502ff573e0117
Birk Johansson <birk@dhis2.org>
2020-09-07 17:44:10 +0200

    fix(scroll): scroll to top on route change

--- src/components/Router/Router.js
+++ src/components/Router/Router.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Redirect, Route, Switch } from 'react-router-dom'
-
+import { ScrollToTop } from '../Router/ScrollToTop'
 import {
     DataImport,
     DataExport,
@@ -17,23 +17,25 @@ import {
 
 const Router = () => {
     return (
-        <Switch>
-            <Route path="/import/data" component={DataImport} />
-            <Route path="/import/event" component={EventImport} />
-            <Route path="/import/gml" component={GMLImport} />
-            <Route path="/import/metadata" component={MetadataImport} />
-            <Route path="/import/tei" component={TEIImport} />
-            <Route path="/export/data" component={DataExport} />
-            <Route path="/export/event" component={EventExport} />
-            <Route
-                path="/export/metadata-dependency"
-                component={MetadataDependencyExport}
-            />
-            <Route path="/export/metadata" component={MetadataExport} />
-            <Route path="/export/tei" component={TEIExport} />
-            <Route path="/utils/job-overview" component={JobOverview} />
-            <Redirect from="*" to="/import/data" />
-        </Switch>
+        <ScrollToTop>
+            <Switch>
+                <Route path="/import/data" component={DataImport} />
+                <Route path="/import/event" component={EventImport} />
+                <Route path="/import/gml" component={GMLImport} />
+                <Route path="/import/metadata" component={MetadataImport} />
+                <Route path="/import/tei" component={TEIImport} />
+                <Route path="/export/data" component={DataExport} />
+                <Route path="/export/event" component={EventExport} />
+                <Route
+                    path="/export/metadata-dependency"
+                    component={MetadataDependencyExport}
+                />
+                <Route path="/export/metadata" component={MetadataExport} />
+                <Route path="/export/tei" component={TEIExport} />
+                <Route path="/utils/job-overview" component={JobOverview} />
+                <Redirect from="*" to="/import/data" />
+            </Switch>
+        </ScrollToTop>
     )
 }
 
---
+++ src/components/Router/ScrollToTop.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { useHistory } from 'react-router-dom'
+
+export const ScrollToTop = ({ children }) => {
+    const history = useHistory()
+    useEffect(() => {
+        const unlisten = history.listen(() => {
+            document.getElementsByClassName('app-shell-app')[0]?.scrollTo(0, 0)
+        })
+
+        return () => {
+            unlisten()
+        }
+    }, [])
+
+    return <>{children}</>
+}
+
+ScrollToTop.propTypes = {
+    children: PropTypes.node,
+}
