90d56a50f1e1102cd826179e30a9d4b14d3882ae
Wahed Mangal <mangal.wahed@gmail.com>
2020-09-10 12:24:32 +0200

    feat: show a `job started` alert when a job is started without error

--- src/utils/final-form.js
+++ src/utils/final-form.js
@@ -1,2 +1,14 @@
+import i18n from '@dhis2/d2-i18n'
+
 export const FORM_ERROR = 'FINAL_FORM/form-error'
 export const ARRAY_ERROR = 'FINAL_FORM/array-error'
+
+export const jobStartedMessage = {
+    [FORM_ERROR]: [
+        {
+            id: `job-started`,
+            info: true,
+            message: i18n.t('Job started.'),
+        },
+    ],
+}
