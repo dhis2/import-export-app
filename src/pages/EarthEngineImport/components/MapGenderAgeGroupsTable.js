f5915fa97d747e33b8b1ff03c5bd5121ec150051
Jen Jones Arnesen <jennifer@dhis2.org>
2022-08-12 11:43:23 +0200

    fix: no validation on bands, and defaultValue

--- src/pages/EarthEngineImport/components/MapGenderAgeGroupsTable.js
+++ src/pages/EarthEngineImport/components/MapGenderAgeGroupsTable.js
@@ -10,7 +10,6 @@ import {
     TableCell,
     NoticeBox,
     SingleSelectFieldFF,
-    hasValue,
 } from '@dhis2/ui'
 import React from 'react'
 import { useCachedDataQuery } from '../util/CachedQueryProvider.js'
@@ -119,12 +118,11 @@ const MappingTable = () => {
                                         inputWidth="250px"
                                         filterable
                                         clearable
-                                        // initialValue={getProbableCocMatch(
-                                        //     bandId
-                                        // )}
+                                        defaultValue={getProbableCocMatch(
+                                            bandId
+                                        )}
                                         noMatchText={i18n.t('No match found')}
                                         options={getCatComboOptions(bandId)}
-                                        validate={hasValue}
                                     />
                                 </TableCell>
                             </TableRow>
